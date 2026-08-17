70aac64b965739e1b07aafd81101d2689bd14f4d
bmtonga <mtongabernard6@gmail.com>
2026-08-17 18:00:12 +0200

    feat: configure Django URL routing and React App component routes for authentication and dealership views

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,8 @@
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 
 
 function App() {
@@ -8,6 +10,9 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
+      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+
     </Routes>
   );
 }
